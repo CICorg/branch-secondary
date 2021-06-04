da3912bde52df2c9fff23379d196fb3dad1a277b
deepakvishwakarma-hh <deepakvish7354@gmail.com>
2021-06-04 22:24:55 +0530

    close

--- javascript/articlefunc.js
+++ javascript/articlefunc.js
@@ -17,11 +17,31 @@ let articleBody05 = document.getElementById('articleBody_05');
 /* Filtered Article refer objects*/
 
 let article01 = {
+  head: 'What is HTML sementic?',
+  body: 'Semantic HTML is the use of HTML markup to reinforce the semantics, or meaning, of the information in webpages and web applications rather than merely to define its presentation or look. Semantic HTML is processed by traditional web browsers as well as by many other user agents.',
+}
+let article02 = {
+  head: 'What is HTML?',
+  body: 'Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.'
+}
+let article03 = {
+  head: 'What is HTML?',
+  body: 'Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.'
+}
+let article04 = {
+  head: 'What is HTML?',
+  body: 'Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.'
+}
+let article05 = {
   head: 'What is HTML?',
   body: 'Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.'
 }
 
 /* Putting value on Articles*/
-
+// Article 1 
 articleHead01.innerHTML = article01.head;
 articleBody01.innerHTML = article01.body;
+
+
+
+// copy codef
