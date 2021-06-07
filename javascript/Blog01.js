b6797549c3f8f17144cb62a4511e7f8f09f49808
DOM-HH <dommod7000@gmail.com>
2021-06-07 21:03:17 +0530

    7June

--- javascript/Blog01.js
+++ javascript/Blog01.js
@@ -6,4 +6,5 @@ let AP = document.getElementById('ArticlePara');
 // Value Putting
 AH.innerHTML = "Bootstrap responsive web layout";
 AU.innerHTML = "Last Updated : 22 Mar, 2021";
-AP.innerHTML = "Bootstrap is a free and open-source tool collection for creating responsive websites and web applications. It is the most popular HTML, CSS, and JavaScript framework for developing responsive, mobile-first websites. Nowadays, the websites are perfect for all the browsers (IE, Firefox, and Chrome) and for all sizes of screens (Desktop, Tablets, Phablets, and Phones). All thanks to Bootstrap developers – Mark Otto and Jacob Thornton of Twitter, though it was later declared to be an open-source project.";
+AP.innerHTML = "Bootstrap is a free and open-source tool collection for creating responsive websites and web applications. It is the most popular HTML, CSS, and JavaScript framework for developing responsive, mobile-first websites. therefore now we create basic (simplistic) Bootstrap layout.";
+
