726cb52ba2f414bce667ecec30a8416e7a0b7caa
DOM-HH <dommod7000@gmail.com>
2021-06-08 07:43:06 +0530

    Re-closed

--- javascript/Blog01.js
+++ javascript/Blog01.js
@@ -1,10 +1,30 @@
 // DOM References
-let AH = document.getElementById('ArticleHeader');
-let AU = document.getElementById('ArticleUpdate');
-let AP = document.getElementById('ArticlePara');
+let articleHeader = document.getElementById('ArticleHeader');
+let articleUpdate = document.getElementById('ArticleUpdate');
+let articleParagraph = document.getElementById('ArticlePara');
+let relatedImg = document.getElementById('article-img');
+let liHeader = document.getElementById('li-header');
+
 
 // Value Putting
-AH.innerHTML = "Bootstrap responsive web layout";
-AU.innerHTML = "Last Updated : 22 Mar, 2021";
-AP.innerHTML = "Bootstrap is a free and open-source tool collection for creating responsive websites and web applications. It is the most popular HTML, CSS, and JavaScript framework for developing responsive, mobile-first websites. therefore now we create basic (simplistic) Bootstrap layout.";
+articleHeader.innerHTML = "Bootstrap responsive web layout";
+articleUpdate.innerHTML = "Last Updated : 22 Mar, 2021";
+articleParagraph.innerHTML = "Bootstrap is a free and open-source tool collection for creating responsive websites and web applications. It is the most popular HTML, CSS, and JavaScript framework for developing responsive, mobile-first websites. therefore now we create basic (simplistic) Bootstrap layout.";
+liHeader.innerHTML = "Why we use Bootstrap?";
+relatedImg.src = "../media/M.png";
 
+
+
+
+
+
+
+// function 
+function showcode() {
+  var x = document.getElementById("layoutCode");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
