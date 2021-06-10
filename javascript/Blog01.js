21b7f030566df5d82b2836f6773fd5f9bbf5adb6
DOM-HH <dommod7000@gmail.com>
2021-06-10 10:20:07 +0530

    10june

--- javascript/Blog01.js
+++ javascript/Blog01.js
@@ -11,20 +11,32 @@ articleHeader.innerHTML = "Bootstrap responsive web layout";
 articleUpdate.innerHTML = "Last Updated : 22 Mar, 2021";
 articleParagraph.innerHTML = "Bootstrap is a free and open-source tool collection for creating responsive websites and web applications. It is the most popular HTML, CSS, and JavaScript framework for developing responsive, mobile-first websites. therefore now we create basic (simplistic) Bootstrap layout.";
 liHeader.innerHTML = "Why we use Bootstrap?";
-relatedImg.src = "../media/M.png";
+relatedImg.src = "../media/bootstrap.jpeg";
 
 
+// load
 
+// menu bar : animation and search bar 
+var x = document.getElementById('blogmenubar');
+x.style.display = "none";
 
-
-
-
-// function 
-function showcode() {
-  var x = document.getElementById("layoutCode");
-  if (x.style.display == "none") {
-    x.style.display = "block";
+function show() {
+  if (x.style.display == 'none') {
+    x.style.display = 'block';
   } else {
-    x.style.display = "none";
+    x.style.display = 'none';
+  }
+  document.getElementById('menubarInner').classList.add('blogmenubarAni');
+}
+var y = document.getElementById('sb');
+y.style.display = "none";
+
+function search() {
+  if (y.style.display == 'none') {
+    y.style.display = 'block';
+  } else {
+    y.style.display = 'none';
   }
 }
+
+// ------------------------
